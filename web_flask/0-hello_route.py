d7982616615f940ac2a06f30305691b0d428c122
John Nyokabi <jwnyokabi@gmail.com>
2023-05-22 18:23:04 +0300

    Application server

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Script that starts a Flask web application"""
-from flask import Flask, render_template
+from flask import Flask
  
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/airbnb-onepage/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
-    return render_template('10-hbnb_filters.html', 'Hello HBNB!')
+    return 'Hello HBNB!'
 
 
 if __name__ == "__main__":
